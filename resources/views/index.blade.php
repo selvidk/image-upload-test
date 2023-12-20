950dda9cd5bab0ad9928c7b0223d99bf2bb427da
selvidk <selvi.dwikartika30@gmail.com>
2023-12-20 18:55:08 +0700

    7th update: title modal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -100,7 +100,7 @@
                     <div class="modal-dialog modal-dialog-centered">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title">Image Delete</h5>
+                                <h5 class="modal-title">Update Image</h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                     aria-label="Close"></button>
                             </div>
